0303767dfb63713f6c10ca2e755ed9963324f706
Luis Carlos <luisscarlostec@gmail.com>
2022-10-31 23:06:32 -0300

    first commit

---
+++ src/main/java/com/contabilidade/facil/ContabilidadeFacilApplication.java
@@ -0,0 +1,13 @@
+package com.contabilidade.facil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilidadeFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabilidadeFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contabilidade/facil/ContabilidadeFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contabilidade.facil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabilidadeFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
